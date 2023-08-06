e323a907b854edc5f5d962c4e8cebd6b824f7e9b
castilloje <subscriber.acct@gmail.com>
2023-08-05 21:28:15 -0400

    Log .env variables

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@
 const nextConfig = {}
 
 console.log('API URL:', process.env.NEXT_PUBLIC_API_URL);
+console.log('Member Stack:', process.env.NEXT_PUBLIC_MEMBERSTACK_KEY);
 
 module.exports = nextConfig
